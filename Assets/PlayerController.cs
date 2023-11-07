82012a1f13be97c5d1c1598df29b2efba5271a8b
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-11-07 19:29:42 +0900

    プレイヤーがジャンプする

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,30 +6,47 @@ public class PlayerController : MonoBehaviour
 {
     //移動速度
     public float moveSpeed = 5.0f;
+    //ジャンプ力
+    public float jumpPower = 3.0f;
     //マウス(視点)感度
     public float sensitivity = 2.0f;
 
     private CharacterController controller;
     private float rotationX = 0;
 
+    private Vector3 vector3;
+    private Transform _transform;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        
+        _transform = transform;
+
         //マウスカーソルを画面内にロック
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        
         // キーの入力軸を取得
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         //入力をワールド座標からローカル座標に変換
         Vector3 moveDirection = transform.TransformDirection(new Vector3(horizontal, 0, vertical) * moveSpeed);
+        if (Input.GetButtonDown("Jump"))
+        {
+           vector3.y = jumpPower;
+        }
+        else
+        {
+            vector3.y += Physics.gravity.y * Time.deltaTime;
+        }
+
+
         //フレームレート調整
         controller.Move(moveDirection * Time.deltaTime);
-
+        controller.Move(vector3 * Time.deltaTime);
         // マウスの入力軸を取得
         float mouseX = Input.GetAxis("Mouse X") * sensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
@@ -44,5 +61,7 @@ public class PlayerController : MonoBehaviour
         Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
         //プレイヤーの水平回転を制御
         transform.rotation *= Quaternion.Euler(0, mouseX, 0);
+
+       
     }
 }
