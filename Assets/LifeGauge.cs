6d6c054684f27eef6ef83cb2677ab3ed7cb919b0
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-11-15 19:56:10 +0900

    敵の頭上にライフゲージを出すテスト

---
+++ Assets/LifeGauge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeGauge : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform parentRectTransform;
+
+    [SerializeField]
+    Transform Enemytransform;
+
+   
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(Enemytransform.position);
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
+
+        transform.localPosition = localPoint + new Vector2(0, 60);
+    }
+}
